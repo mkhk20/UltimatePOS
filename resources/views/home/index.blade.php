be0560dac1162bac0390a72cbbc93c478e8a9277
wael-Fadlallah <wael.fudlallah@gmail.com>
2021-01-04 01:08:52 -0800

    change dashboard cards

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -49,69 +49,74 @@
     		</div>
     	</div>
     	<br>
-    	<div class="row row-custom">
-        	<div class="col-md-3 col-sm-6 col-xs-12 col-custom">
-    	      <div class="info-box info-box-new-style">
-    	        <span class="info-box-icon bg-aqua"><i class="ion ion-cash"></i></span>
+      <div class="row">
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-body">
+                    <div class="float-right mt-2">
+                        <div id="total-revenue-chart"></div>
+                    </div>
+                    <div>
+                        <h4 class="mb-1 mt-1">$<span data-plugin="counterup">34,152</span></h4>
+                        <p class="text-muted mb-0">Total Revenue</p>
+                    </div>
+                    <p class="text-muted mt-3 mb-0"><span class="text-success mr-1"><i class="mdi mdi-arrow-up-bold ml-1"></i>2.65%</span> since last week
+                    </p>
+                </div>
+            </div>
+        </div> <!-- end col-->
+    
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-body">
+                    <div class="float-right mt-2">
+                        <div id="orders-chart"> </div>
+                    </div>
+                    <div>
+                        <h4 class="mb-1 mt-1"><span data-plugin="counterup">5,643</span></h4>
+                        <p class="text-muted mb-0">Orders</p>
+                    </div>
+                    <p class="text-muted mt-3 mb-0"><span class="text-danger mr-1"><i class="mdi mdi-arrow-down-bold ml-1"></i>0.82%</span> since last week
+                    </p>
+                </div>
+            </div>
+        </div> <!-- end col-->
+    
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-body">
+                    <div class="float-right mt-2">
+                        <div id="customers-chart"> </div>
+                    </div>
+                    <div>
+                        <h4 class="mb-1 mt-1"><span data-plugin="counterup">45,254</span></h4>
+                        <p class="text-muted mb-0">Customers</p>
+                    </div>
+                    <p class="text-muted mt-3 mb-0"><span class="text-danger mr-1"><i class="mdi mdi-arrow-down-bold ml-1"></i>6.24%</span> since last week
+                    </p>
+                </div>
+            </div>
+        </div> <!-- end col-->
+    
+        <div class="col-md-3">
+    
+            <div class="card">
+                <div class="card-body">
+                    <div class="float-right mt-2">
+                        <div id="growth-chart"></div>
+                    </div>
+                    <div>
+                        <h4 class="mb-1 mt-1">+ <span data-plugin="counterup">12.58</span>%</h4>
+                        <p class="text-muted mb-0">Growth</p>
+                    </div>
+                    <p class="text-muted mt-3 mb-0"><span class="text-success mr-1"><i class="mdi mdi-arrow-up-bold ml-1"></i>10.51%</span> since last week
+                    </p>
+                </div>
+            </div>
+        </div> <!-- end col-->
+      </div> <!-- end row-->
+        
 
-    	        <div class="info-box-content">
-    	          <span class="info-box-text">{{ __('home.total_purchase') }}</span>
-    	          <span class="info-box-number total_purchase"><i class="fas fa-sync fa-spin fa-fw margin-bottom"></i></span>
-    	        </div>
-    	        <!-- /.info-box-content -->
-    	      </div>
-    	      <!-- /.info-box -->
-    	    </div>
-    	    <!-- /.col -->
-    	    <div class="col-md-3 col-sm-6 col-xs-12 col-custom">
-    	      <div class="info-box info-box-new-style">
-    	        <span class="info-box-icon bg-aqua"><i class="ion ion-ios-cart-outline"></i></span>
-
-    	        <div class="info-box-content">
-    	          <span class="info-box-text">{{ __('home.total_sell') }}</span>
-    	          <span class="info-box-number total_sell"><i class="fas fa-sync fa-spin fa-fw margin-bottom"></i></span>
-    	        </div>
-    	        <!-- /.info-box-content -->
-    	      </div>
-    	      <!-- /.info-box -->
-    	    </div>
-    	    <!-- /.col -->
-    	    <div class="col-md-3 col-sm-6 col-xs-12 col-custom">
-    	      <div class="info-box info-box-new-style">
-    	        <span class="info-box-icon bg-yellow">
-    	        	<i class="fa fa-dollar"></i>
-    				<i class="fa fa-exclamation"></i>
-    	        </span>
-
-    	        <div class="info-box-content">
-    	          <span class="info-box-text">{{ __('home.purchase_due') }}</span>
-    	          <span class="info-box-number purchase_due"><i class="fas fa-sync fa-spin fa-fw margin-bottom"></i></span>
-    	        </div>
-    	        <!-- /.info-box-content -->
-    	      </div>
-    	      <!-- /.info-box -->
-    	    </div>
-    	    <!-- /.col -->
-
-    	    <!-- fix for small devices only -->
-    	    <!-- <div class="clearfix visible-sm-block"></div> -->
-    	    <div class="col-md-3 col-sm-6 col-xs-12 col-custom">
-    	      <div class="info-box info-box-new-style">
-    	        <span class="info-box-icon bg-yellow">
-    	        	<i class="ion ion-ios-paper-outline"></i>
-    	        	<i class="fa fa-exclamation"></i>
-    	        </span>
-
-    	        <div class="info-box-content">
-    	          <span class="info-box-text">{{ __('home.invoice_due') }}</span>
-    	          <span class="info-box-number invoice_due"><i class="fas fa-sync fa-spin fa-fw margin-bottom"></i></span>
-    	        </div>
-    	        <!-- /.info-box-content -->
-    	      </div>
-    	      <!-- /.info-box -->
-    	    </div>
-    	    <!-- /.col -->
-      	</div>
       	<div class="row row-custom">
             <!-- expense -->
             <div class="col-md-3 col-sm-6 col-xs-12 col-custom">
@@ -299,6 +304,278 @@
       var chart = new ApexCharts(document.querySelector("#apexcharts"), options);
 
       chart.render();
+    </script>
+
+    {{-- charts --}}
+    <script>
+      //
+      // Total Revenue Chart
+      //
+      var options1 = {
+          series: [{
+              data: [25, 66, 41, 89, 63, 25, 44, 20, 36, 40, 54]
+          }],
+          fill: {
+              colors: ['#5b73e8']
+          },
+          chart: {
+              type: 'bar',
+              width: 70,
+              height: 40,
+              sparkline: {
+                  enabled: true
+              }
+          },
+          plotOptions: {
+              bar: {
+                  columnWidth: '50%'
+              }
+          },
+          labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+          xaxis: {
+              crosshairs: {
+                  width: 1
+              },
+          },
+          tooltip: {
+              fixed: {
+                  enabled: false
+              },
+              x: {
+                  show: false
+              },
+              y: {
+                  title: {
+                      formatter: function (seriesName) {
+                          return ''
+                      }
+                  }
+              },
+              marker: {
+                  show: false
+              }
+          }
+      };
+
+      var chart1 = new ApexCharts(document.querySelector("#total-revenue-chart"), options1);
+      chart1.render();
+
+      //
+      // Orders Chart
+      //
+      var options = {
+          fill: {
+              colors: ['#34c38f']
+          },
+          series: [70],
+          chart: {
+              type: 'radialBar',
+              width: 45,
+              height: 45,
+              sparkline: {
+                  enabled: true
+              }
+          },
+          dataLabels: {
+              enabled: false
+          },
+          plotOptions: {
+              radialBar: {
+                  hollow: {
+                      margin: 0,
+                      size: '60%'
+                  },
+                  track: {
+                      margin: 0
+                  },
+                  dataLabels: {
+                      show: false
+                  }
+              }
+          }
+      };
+
+      var chart = new ApexCharts(document.querySelector("#orders-chart"), options);
+      chart.render();
+
+
+      //
+      // Customers Chart
+      //
+
+      var options = {
+          fill: {
+              colors: ['#5b73e8']
+          },
+          series: [55],
+          chart: {
+              type: 'radialBar',
+              width: 45,
+              height: 45,
+              sparkline: {
+                  enabled: true
+              }
+          },
+          dataLabels: {
+              enabled: false
+          },
+          plotOptions: {
+              radialBar: {
+                  hollow: {
+                      margin: 0,
+                      size: '60%'
+                  },
+                  track: {
+                      margin: 0
+                  },
+                  dataLabels: {
+                      show: false
+                  }
+              }
+          }
+      };
+
+      var chart = new ApexCharts(document.querySelector("#customers-chart"), options);
+      chart.render();
+
+
+      //
+      // Growth Chart
+      //
+      var options2 = {
+          series: [{
+              data: [25, 66, 41, 89, 63, 25, 44, 12, 36, 9, 54]
+          }],
+          fill: {
+              colors: ['#f1b44c']
+          },
+          chart: {
+              type: 'bar',
+              width: 70,
+              height: 40,
+              sparkline: {
+                  enabled: true
+              }
+          },
+          plotOptions: {
+              bar: {
+                  columnWidth: '50%'
+              }
+          },
+          labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+          xaxis: {
+              crosshairs: {
+                  width: 1
+              },
+          },
+          tooltip: {
+              fixed: {
+                  enabled: false
+              },
+              x: {
+                  show: false
+              },
+              y: {
+                  title: {
+                      formatter: function (seriesName) {
+                          return ''
+                      }
+                  }
+              },
+              marker: {
+                  show: false
+              }
+          }
+      };
+
+      var chart2 = new ApexCharts(document.querySelector("#growth-chart"), options2);
+      chart2.render();
+
+
+      //
+      // Sales Analytics Chart
+
+      var options = {
+          chart: {
+              height: 339,
+              type: 'line',
+              stacked: false,
+              toolbar: {
+                  show: false
+              }
+          },
+          stroke: {
+              width: [0, 2, 4],
+              curve: 'smooth'
+          },
+          plotOptions: {
+              bar: {
+                  columnWidth: '30%'
+              }
+          },
+          colors: ['#5b73e8', '#dfe2e6', '#f1b44c'],
+          series: [{
+              name: 'Desktops',
+              type: 'column',
+              data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30]
+          }, {
+              name: 'Laptops',
+              type: 'area',
+              data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43]
+          }, {
+              name: 'Tablets',
+              type: 'line',
+              data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39]
+          }],
+          fill: {
+              opacity: [0.85, 0.25, 1],
+              gradient: {
+                  inverseColors: false,
+                  shade: 'light',
+                  type: "vertical",
+                  opacityFrom: 0.85,
+                  opacityTo: 0.55,
+                  stops: [0, 100, 100, 100]
+              }
+          },
+          labels: ['01/01/2003', '02/01/2003', '03/01/2003', '04/01/2003', '05/01/2003', '06/01/2003', '07/01/2003', '08/01/2003', '09/01/2003', '10/01/2003', '11/01/2003'],
+          markers: {
+              size: 0
+          },
+
+          xaxis: {
+              type: 'datetime'
+          },
+          yaxis: {
+              title: {
+                  text: 'Points',
+              },
+          },
+          tooltip: {
+              shared: true,
+              intersect: false,
+              y: {
+                  formatter: function (y) {
+                      if (typeof y !== "undefined") {
+                          return y.toFixed(0) + " points";
+                      }
+                      return y;
+
+                  }
+              }
+          },
+          grid: {
+              borderColor: '#f1f1f1'
+          }
+        }
+
+        var chart = new ApexCharts(
+          document.querySelector("#sales-analytics-chart"),
+          options
+        );
+
+        chart.render();
+
     </script>
 @endsection
 
--- resources/views/layouts/partials/css.blade.php
+++ resources/views/layouts/partials/css.blade.php
@@ -8,6 +8,7 @@
 
 <!-- app css -->
 <link rel="stylesheet" href="{{ asset('css/app.css?v='.$asset_v) }}">
+<link rel="stylesheet" href="{{ asset('css/addons.css') }}">
 
 @if(isset($pos_layout) && $pos_layout)
 	<style type="text/css">
